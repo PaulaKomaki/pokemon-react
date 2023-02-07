fbc2c9c7855e764165436d6b9fda10597f22add9
Paula Yuri Komaki Da Conceicão <PET0145745@aluno.firjansenaisesi.com.br>
2023-02-06 21:29:43 -0300

    Commit inicial

---
+++ src/pages/home/components.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    h1 {
+        text-align: center;
+        margin: 4rem 0;
+    }
+
+`
+export const PokeList = styled.ul`
+    list-styled: none;
+    display: grid;
+`
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,12 @@
+import { Container } from "./components";
+
+export default function Home(){
+    return (
+        <Container>
+            <h1>Pokemon</h1>
+           
+        
+        </Container>
+    );
+
+}
